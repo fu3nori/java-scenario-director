4b6c726892098d7354afa5b7b9e1076a84149880
fu3nori <cap.fuminori.usui@gmail.com>
2025-06-28 16:54:07 +0900

    first-commit

---
+++ director/src/main/java/com/example/director/DirectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.director;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DirectorApplication.class, args);
+    }
+
+}
---
+++ director/src/test/java/com/example/director/DirectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.director;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
